c84049ba2a008657a4ff9277ee88b697fbb564a5
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-27 10:00:55 +0800

    added scaffold for tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_014903) do
+ActiveRecord::Schema.define(version: 2021_07_27_015827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2021_07_27_014903) do
     t.integer "user_id", null: false
     t.string "title", null: false
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "title", null: false
+    t.text "body", null: false
+    t.date "deadline", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
