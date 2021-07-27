c9bfaa2cc8db5b59f65c357cc938391ffc0e91c7
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-27 11:52:00 +0800

    added table for tasks index

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :categories, dependent: :destroy
+  has_many :tasks, through: :categories
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   resources :tasks
+  resources :categories
   devise_for :users
   root to: 'home#index'
 end
