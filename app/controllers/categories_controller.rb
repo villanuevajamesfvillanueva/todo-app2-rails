da849b73d048db861be8696df0adfce6f5137ea1
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-28 23:39:17 +0800

    progress in layout

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
+
+    def index
+        @categories = current_user.categories.order("updated_at DESC")
+
+    end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -65,6 +65,6 @@ class TasksController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def task_params
-    params.fetch(:task, {})
+    params.require(:task).permit(:category_id, :title, :body, :deadline, :status)
   end
 end
