7ebd804c5f2b458eda3cf34d63c4dd6b1a0dff69
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-07-31 22:38:57 +0800

    added create and delete category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,29 @@
 class CategoriesController < ApplicationController
   def index
     @categories = current_user.categories.order('updated_at DESC')
+    @category = Category.new
+  end
+
+  def create 
+    @category = current_user.categories.build(category_params)
+
+    if @category.valid?
+        @category.save
+        redirect_to request.referrer, notice: 'Category was created successfully.'
+        
+    else
+        render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to request.referrer, notice: 'Category was deleted successfully.'
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:user_id, :title, :description)
   end
 end
