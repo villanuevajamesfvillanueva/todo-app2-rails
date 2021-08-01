0b8eeaf53105e6da3db6452f905687438277d50d
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-08-01 13:22:42 +0800

    added firstname and lastname in signup, sidebar and navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :firstname, :lastname, presence: true
+  # validate :avatar_filesize
+  # has_one_attached :avatar
+
   has_many :categories, dependent: :destroy
   has_many :tasks, through: :categories
 
@@ -20,4 +24,15 @@ class User < ApplicationRecord
   def completed
     tasks.where(status: 'completed').order('deadline ASC')
   end
+
+
+#   private
+
+#   def avatar_filetype
+#     errors.add(:avatar, 'needs to be of the following format: JPEG, JPG, or PNG') if avatar.content_type.in?(%('image/jpeg image/jpg image/png image/JPEG image/JPG image/PNG'))
+#   end
+
+#   def avatar_filesize
+#     errors.add(:avatar, 'file size should not exceed 1 MB!') if avatar.attached? && avatar.byte_size >= 1.megabyte
+#   end
 end
