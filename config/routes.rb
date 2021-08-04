5a25a0aeaccdcf72b5e2b45c441cbd0830e59e7a
James Villanueva <villanuevajamesfvillanueva@gmail.com>
2021-08-04 11:02:53 +0800

    fixed bug showing alert when first time visit to sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,17 @@ Rails.application.routes.draw do
   resources :tasks
   resources :categories
   devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+        root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+        root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   root to: 'home#index'
 
   patch 'tasks/update_status/:id', to: 'tasks#update_status', as: 'update_status'
